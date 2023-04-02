096dffe60f6f8463652dbf98094bef592fb80604
ASIF ANAM SHAON <anam1828@cseku.ac.bd>
2023-04-03 00:49:48 +0600

    array

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+
 'use strict';
 
 //---------------------------------------------------------
+/*
 let hasDriversLicesce = false
 const passTest = true
 if (passTest) hasDriversLicesce = true
 if (hasDriversLicesce) console.log('I can drive a car!')
-
+*/
 // const interface  = "audio"
 // const private = "Car"
 /*
@@ -84,19 +86,14 @@ console.log(yearsUntilRetairment(1960, 'Nicki'))
 */
 
 //---------------challenge1----------------------
-
-let average = (score1, score2, score3)=>(score1+score2+score3)/3
-
-
-function teamAverage(dolphine, koala){
-    return (dolphine+koala)/2
+/*
+function average(score1, score2, score3){
+    return (score1+score2+score3)/3
 }
+const avgDolphine = average(44,23,71)
+const avgKoala = average(85,54,41)
 
 function checkWinner(avgDolphoine, avgKoala){
-
-    const avgDolphoine = average(44,23,71)
-
-    const avgKoala = average(65,54,49)
 
     if(avgDolphoine> 2*avgKoala){
         console.log(`Dolphines win (${avgDolphoine} vs ${avgKoala})`)
@@ -106,4 +103,55 @@ function checkWinner(avgDolphoine, avgKoala){
     } 
     else
         console.log('Match draw!')
+}
+checkWinner(avgDolphine,avgKoala)
+checkWinner(average(44,23,71), average(85,54,41))
+
+ */
+//-------------Arrays----------------------------------
+/*
+const friends = ['Asif', 'Anam', 'Shaon']
+console.log(friends)
+console.log(friends.length)
+console.log(friends[friends.length-1])
+friends[2] = 'Zoo'
+console.log(friends)
+
+const me = ['Asif', 'Anam', true, 24]
+console.log(me)
+function calcAge(birthyear){
+    return 2023 - birthyear
+}
+
+const years = [1999,1998,2000,2020]
+const age1 = calcAge(years[0])
+const age2 = calcAge(years[1])
+const age3 = calcAge(years[years.length-1])
+
+console.log(age1,age2,age3)
+const ages = [age1, age2, age3]
+console.log(ages)
+
+ */
+//------------Array methods -----------------------
+const friends = ['Asif', 'Anam', 'Shaon']
+friends.push('Arman') //add new element at the end
+friends.push('Zoo')
+friends.push('Alex')
+console.log(friends)
+
+friends.unshift('Aldo') //add at the begining
+console.log(friends)
+const popped = friends.pop() // remove last element
+console.log(popped)
+const removeFirst = friends.shift() //remove first
+console.log(removeFirst)
+console.log(friends.indexOf('Anam')) //index where element is located
+console.log(friends.indexOf('Aldo'))
+
+console.log(friends.includes('Anam')) //includes(true or false)
+console.log(friends.includes('Aldo'))
+
+if(friends.includes('Zoo')){
+    console.log('You have a friend named Zoo')
 }
